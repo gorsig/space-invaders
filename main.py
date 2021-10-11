96064bbd1d83f3c8c3b1fea463377c869aa79446
gorsig <sigeskog@hotmail.se>
2021-10-11 09:24:31 +0200

    add gameloop and screen size

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import pygame
+
+HEIGHT = 800
+WIDTH = 600
+
+
 def main():
-    pass
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    running = True
+
+    while running:
+        # Process input/events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
 
 
 if __name__ == '__main__':
