1d3131cf8a989fc4db69f701830f4da0c32e3816
gorsig <sigeskog@hotmail.se>
2021-10-11 10:23:50 +0200

    add movement to the players ship

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ def main():
     player_img = pygame.image.load('img/battleship.png')
     player_x = 370
     player_y = 480
+    player_x_change = 0
 
-    def player():
-        screen.blit(player_img, (player_x, player_y))
+    def player(x, y):
+        screen.blit(player_img, (x, y))
 
     running = True
     while running:
@@ -23,9 +24,19 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player_x_change = -0.3
+                if event.key == pygame.K_RIGHT:
+                    player_x_change = 0.3
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    player_x_change = 0
+
         screen.fill((0, 0, 0))
 
-        player()
+        player_x += player_x_change
+        player(player_x, player_y)
         pygame.display.update()
 
     pygame.quit()
